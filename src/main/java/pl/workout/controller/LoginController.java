673e0a3d7376ecada4d0518b3e798b11cd180d04
dawd333 <dwitekw@gmail.com>
2019-04-18 10:39:24 +0200

    Some basic css and html for login page

---
+++ src/main/java/pl/workout/controller/LoginController.java
@@ -0,0 +1,13 @@
+package pl.workout.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/")
+    public String getLoginPage(){
+        return "login";
+    }
+}
