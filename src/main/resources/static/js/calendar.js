bfbfb79ef009ec4868b4b084efd8e37903825c47
dawd333 <dwitekw@gmail.com>
2019-05-30 09:06:20 +0200

    logout

--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -5,6 +5,48 @@ if(localStorage.getItem(ACCESS_TOKEN) === null) {
     window.location.href = '/login';
 }
 
+const request = (options) => {
+    const headers = new Headers({
+        'Content-Type': 'application/json',
+    });
+
+    if(localStorage.getItem(ACCESS_TOKEN)) {
+        headers.append('Authorization', 'Bearer ' + localStorage.getItem(ACCESS_TOKEN))
+    }
+
+    const defaults = {headers: headers};
+    options = Object.assign({}, defaults, options);
+
+    return fetch(options.url, options)
+        .then(response =>
+            response.json().then(json => {
+                if(!response.ok) {
+                    return Promise.reject(json);
+                }
+                return json;
+            })
+        );
+};
+
+function logout() {
+    return request({
+        url: API_BASE_URL + "/auth/logout",
+        method: 'GET'
+    });
+}
+
+function logoutButton() {
+    logout()
+        .then(response => {
+            localStorage.removeItem(ACCESS_TOKEN);
+            console.log("Logged out successfully.");
+            console.log(response);
+            window.location.href = "/login"
+        }).catch(error => {
+        console.log("Please try again :(");
+        console.log(error);
+    });
+}
 //begin of calendar code
 
 var categoriesDict = {};
