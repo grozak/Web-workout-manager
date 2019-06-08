fc485c2d41504cd0766cd10df38686b79735020e
dawd333 <dwitekw@gmail.com>
2019-06-08 19:00:35 +0200

    xd

--- src/main/resources/static/js/friend_calendar.js
+++ src/main/resources/static/js/friend_calendar.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://web-workout-manager.herokuapp.com/api';
+const API_BASE_URL = '/api';
 const ACCESS_TOKEN = 'accessToken';
 
 if(localStorage.getItem(ACCESS_TOKEN) === null) {
